4d18708d0ab2578d01a55b0502c7081b937fa390
Mysta3 <myshawnestallings@gmail.com>
2020-02-14 09:35:00 -0800

    added PUT/DELETE tests

--- test/ibcc.test.js
+++ test/ibcc.test.js
@@ -6,6 +6,8 @@ const should = require('chai').should();
 const api = supertest('http://localhost:3001');
 
 // Events test
+
+//SHOW ALL
 describe('GET /events', () => {
   it('should return all events', done => {
     api
@@ -18,6 +20,7 @@ describe('GET /events', () => {
   });
 });
 
+//SHOW BY SPECIFIC
 describe('GET /events/:name', () => {
   it('should return an event with a specific name', done => {
     api
@@ -27,5 +30,97 @@ describe('GET /events/:name', () => {
         expect(res.body).to.be.an('Array');
         done();
       });
+  });
+});
+
+//CREATE
+describe('POST /events', () => {
+  let newEvent = {
+    name: 'TestEvent',
+    time: '12:00am',
+    timezone: 'PST',
+    price: 80
+  };
+
+  before(done => {
+    api
+      .post('/events')
+      .set('Accept', 'application/json')
+      .send(newEvent)
+      .end(done);
+  });
+
+  it('Should add a new event to the collection and return it', done => {
+    api
+      .get('/events')
+      .set('Accept', 'application/json')
+      .end((err, res) => {
+        const eventToFind = res.body.find(
+          event => event.name === newEvent.name
+        );
+        expect(eventToFind).to.be.an('object');
+        done();
+      });
+  });
+});
+
+//DELETE
+describe('DELETE /events/:name ', () => {
+  let eventToDelete;
+  before(done => {
+    api
+      .get('/events')
+      .set('Accept', 'application/json')
+      .end((err, res) => {
+        const events = res.body;
+        eventToDelete = events[events.length - 1].name;
+        done();
+      });
+  });
+
+  before(done => {
+    api
+      .delete(`/events/${eventToDelete}`)
+      .set('Accept', 'application/json')
+      .end((err, res) => {
+        done();
+      });
+  });
+
+  it('should remove event from original array', done => {
+    api
+      .get('/events')
+      .set('Accept', 'application/json')
+      .end((err, res) => {
+        const deleteEvent = res.body.find(event => event.name === eventToDelete);
+        expect(deleteEvent).to.equal(undefined);
+        done();
+      });
+  });
+});
+
+//PUT
+describe('PUT /events/:name', () => {
+  let updateEvent = {
+    name: 'TestEvent',
+    time: '5:00pm'
+  };
+
+  before(done => {
+    api
+      .put(`/events/${updateEvent.name}`)
+      .set('Accept', 'application/json')
+      .send(updateEvent)
+      .end(done);
+  });
+
+  it('should update event by name', done => {
+    api
+      .get(`/events/${updateEvent.name}`)
+      .set('Accept', 'application/json')
+      .end((err, res) => {
+        expect(res.body.name).to.equal(updateEvent.name);
+      });
+    done();
   });
 });

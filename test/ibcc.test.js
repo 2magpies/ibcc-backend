fb4f5107238bb0b78535d1474e5897859bdff25c
tiffhuddleston <tiffhuddleston@gmail.com>
2020-02-14 10:54:40 -0600

    added show route test

--- test/ibcc.test.js
+++ test/ibcc.test.js
@@ -1,6 +1,8 @@
 const expect = require('chai').expect;
 const supertest = require('supertest');
 
+const should = require('chai').should();
+
 const api = supertest('http://localhost:3001');
 
 // Events test
@@ -8,7 +10,19 @@ describe('GET /events', () => {
   it('should return all events', done => {
     api
       .get('/events')
-      .set('Accept', 'applications/json')
+      .set('Accept', 'application/json')
+      .end((err, res) => {
+        expect(res.body).to.be.an('Array');
+        done();
+      });
+  });
+});
+
+describe('GET /events/:name', () => {
+  it('should return an event with a specific name', done => {
+    api
+      .get('/events/Carnival')
+      .set('Accept', 'application/json')
       .end((err, res) => {
         expect(res.body).to.be.an('Array');
         done();

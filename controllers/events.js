a0beeda68c342e34ead0786480403f489e5267cd
Mysta3 <myshawnestallings@gmail.com>
2020-02-17 12:13:35 -0800

    fixed img schema

--- controllers/events.js
+++ controllers/events.js
@@ -4,31 +4,33 @@ const Events = require('../models/Events');
 
 //SHOW
 router.get('/', (req, res) => {
-    Events.find({}).then(event => res.json(event));
+  Events.find({}).then(event => res.json(event));
 });
 
 //VIEW
 router.get('/:name', (req, res) => {
-    Events.find({ name: req.params.name }).then(event => res.json(event))
+  Events.find({ name: req.params.name }).then(event => res.json(event));
 });
 
 //CREATE
 router.post('/', (req, res) => {
-    let newEvents = req.body;
-    Events.create(newEvents).then(event => res.json(event));
+  let newEvents = req.body;
+  Events.create(newEvents).then(event => res.json(event));
 });
 
 //UPDATE
 router.put('/:name', (req, res) => {
-    let updatedEvents = req.body;
-    Events.findOneAndUpdate({ name: req.params.name }, updatedEvents, {
-        new: true
-    }).then(event => res.json(event));
+  let updatedEvents = req.body;
+  Events.findOneAndUpdate({ name: req.params.name }, updatedEvents, {
+    new: true
+  }).then(event => res.json(event));
 });
 
 //DELETE
 router.delete('/:name', (req, res) => {
-    Events.findOneAndDelete({ name: req.params.name }).then(event => res.json(event));
+  Events.findOneAndDelete({ name: req.params.name }).then(event =>
+    res.json(event)
+  );
 });
 
 module.exports = router;

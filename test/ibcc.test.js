2367b1682dffea48f3df8658c4039a5bcc479358
2magpies <daphnemintz@yahoo.com>
2020-02-18 10:33:42 -0500

    Added user GET test

--- test/ibcc.test.js
+++ test/ibcc.test.js
@@ -8,23 +8,132 @@ const api = supertest('http://localhost:3001');
 // Events test
 
 //SHOW ALL
-describe('GET /events', () => {
-  it('should return all events', done => {
-    api
-      .get('/events')
-      .set('Accept', 'application/json')
-      .end((err, res) => {
-        expect(res.body).to.be.an('Array');
-        done();
-      });
-  });
-});
+// describe('GET /events', () => {
+//   it('should return all events', done => {
+//     api
+//       .get('/events')
+//       .set('Accept', 'application/json')
+//       .end((err, res) => {
+//         expect(res.body).to.be.an('Array');
+//         done();
+//       });
+//   });
+// });
 
 //SHOW BY SPECIFIC
-describe('GET /events/:name', () => {
-  it('should return an event with a specific name', done => {
+// describe('GET /events/:name', () => {
+//   it('should return an event with a specific name', done => {
+//     api
+//       .get('/events/Carnival')
+//       .set('Accept', 'application/json')
+//       .end((err, res) => {
+//         expect(res.body).to.be.an('Array');
+//         done();
+//       });
+//   });
+// });
+
+//CREATE
+// describe('POST /events', () => {
+//   let newEvent = {
+//     name: 'TestEvent',
+//     time: '12:00am',
+//     timezone: 'PST',
+//     price: 80
+//   };
+
+//   before(done => {
+//     api
+//       .post('/events')
+//       .set('Accept', 'application/json')
+//       .send(newEvent)
+//       .end(done);
+//   });
+
+//   it('Should add a new event to the collection and return it', done => {
+//     api
+//       .get('/events')
+//       .set('Accept', 'application/json')
+//       .end((err, res) => {
+//         const eventToFind = res.body.find(
+//           event => event.name === newEvent.name
+//         );
+//         expect(eventToFind).to.be.an('object');
+//         done();
+//       });
+//   });
+// });
+
+//DELETE
+// describe('DELETE /events/:name ', () => {
+//   let eventToDelete;
+//   before(done => {
+//     api
+//       .get('/events')
+//       .set('Accept', 'application/json')
+//       .end((err, res) => {
+//         const events = res.body;
+//         eventToDelete = events[events.length - 1].name;
+//         done();
+//       });
+//   });
+
+//   before(done => {
+//     api
+//       .delete(`/events/${eventToDelete}`)
+//       .set('Accept', 'application/json')
+//       .end((err, res) => {
+//         done();
+//       });
+//   });
+
+//   it('should remove event from original array', done => {
+//     api
+//       .get('/events')
+//       .set('Accept', 'application/json')
+//       .end((err, res) => {
+//         const deleteEvent = res.body.find(
+//           event => event.name === eventToDelete
+//         );
+//         expect(deleteEvent).to.equal(undefined);
+//         done();
+//       });
+//   });
+// });
+
+//PUT
+// describe('PUT /events/:name', () => {
+//   let updateEvent = {
+//     name: 'TestEvent',
+//     time: '5:00pm'
+//   };
+
+//   before(done => {
+//     api
+//       .put(`/events/${updateEvent.name}`)
+//       .set('Accept', 'application/json')
+//       .send(updateEvent)
+//       .end(done);
+//   });
+
+//   it('should update event by name', done => {
+//     api
+//       .get(`/events/${updateEvent.name}`)
+//       .set('Accept', 'application/json')
+//       .end((err, res) => {
+//         expect(res.body.name).to.equal(updateEvent.name);
+//       });
+//     done();
+//   });
+// });
+
+// User tests
+
+// Review user (GET)
+describe('GET /users/:name', () => {
+  it('should return a user with a specific name', done => {
     api
-      .get('/events/Carnival')
+      .get('/users/Brendan')
       .set('Accept', 'application/json')
       .end((err, res) => {
         expect(res.body).to.be.an('Array');
@@ -33,96 +142,9 @@ describe('GET /events/:name', () => {
   });
 });
 
-//CREATE
-describe('POST /events', () => {
-  let newEvent = {
-    name: 'TestEvent',
-    time: '12:00am',
-    timezone: 'PST',
-    price: 80
-  };
+// Create new user (POST)
 
-  before(done => {
-    api
-      .post('/events')
-      .set('Accept', 'application/json')
-      .send(newEvent)
-      .end(done);
-  });
 
-  it('Should add a new event to the collection and return it', done => {
-    api
-      .get('/events')
-      .set('Accept', 'application/json')
-      .end((err, res) => {
-        const eventToFind = res.body.find(
-          event => event.name === newEvent.name
-        );
-        expect(eventToFind).to.be.an('object');
-        done();
-      });
-  });
-});
+// Update user (PUT)
 
-//DELETE
-describe('DELETE /events/:name ', () => {
-  let eventToDelete;
-  before(done => {
-    api
-      .get('/events')
-      .set('Accept', 'application/json')
-      .end((err, res) => {
-        const events = res.body;
-        eventToDelete = events[events.length - 1].name;
-        done();
-      });
-  });
-
-  before(done => {
-    api
-      .delete(`/events/${eventToDelete}`)
-      .set('Accept', 'application/json')
-      .end((err, res) => {
-        done();
-      });
-  });
-
-  it('should remove event from original array', done => {
-    api
-      .get('/events')
-      .set('Accept', 'application/json')
-      .end((err, res) => {
-        const deleteEvent = res.body.find(
-          event => event.name === eventToDelete
-        );
-        expect(deleteEvent).to.equal(undefined);
-        done();
-      });
-  });
-});
-
-//PUT
-describe('PUT /events/:name', () => {
-  let updateEvent = {
-    name: 'TestEvent',
-    time: '5:00pm'
-  };
-
-  before(done => {
-    api
-      .put(`/events/${updateEvent.name}`)
-      .set('Accept', 'application/json')
-      .send(updateEvent)
-      .end(done);
-  });
-
-  it('should update event by name', done => {
-    api
-      .get(`/events/${updateEvent.name}`)
-      .set('Accept', 'application/json')
-      .end((err, res) => {
-        expect(res.body.name).to.equal(updateEvent.name);
-      });
-    done();
-  });
-});
+// Delete user (DELETE)
